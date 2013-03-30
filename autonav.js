9714914b6796b4efe32ab60c28e269f909f3ae01
Mike Kamermans <pomax@nihongoresources.com>
2013-03-30 17:45:14 -0400

    t-for-distance

--- autonav.js
+++ autonav.js
@@ -1,9 +1,12 @@
 (function(d) {
   var nav = d.querySelector("#navbar");
   var items = d.querySelectorAll("section"), e, i, l;
+  var sectionId = 1;
   for(i=1, l=items.length; i<l; i++) {
     e = items[i];
     if(e.id) {
+      var titular = e.querySelector("h2");
+      titular.setAttribute("data-num", sectionId++); 
       var entry = d.createElement("li");
       var html = "<a href='#" + e.getAttribute("id") + "'>" + e.querySelector("h2").innerHTML + "</a>";
       entry.innerHTML = html;
