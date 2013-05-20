d577173ee68b8236feecc6e2969cb158c204c8e6
Mike Kamermans <pomax@nihongoresources.com>
2013-05-20 11:04:22 -0400

    fixed nav finding 'undefined' for some sections

--- autonav.js
+++ autonav.js
@@ -2,15 +2,16 @@ schedule(function() {
   var nav = find("#navbar ol"),
       items = find("section"),
       sectionId = 1,
-      e, i, l;
+      e, i, l,
+      id, titular, entry;
   for(i=1, l=items.length; i<l; i++) {
     e = items[i];
     if(e.id) {
-      var titular = e.find("h2");
+      id = e.id,
+      titular = find("#"+id+" > h2:first-child"),
       titular.set("data-num", sectionId++);
-      var entry = create("li");
-      var html = "<a href='#" + e.get("id") + "'>" + e.find("h2").innerHTML + "</a>";
-      entry.innerHTML = html;
+      entry = create("li");
+      entry.innerHTML = "<a href='#" + id + "'>" +titular.innerHTML + "</a>";
       nav.add(entry);
     }
   };
