ea191eba12e874ae2a5a7ca96c16998cdc1eb902
Pomax <pomax@nihongoresources.com>
2016-01-14 09:56:12 -0800

    fix for quad polybezier

--- components/SectionHeader.jsx
+++
@@ -1,9 +0,0 @@
-var React = require("react");
-
-var SectionHeader = React.createClass({
-  render: function() {
-    return <h2 data-num={this.props.number}><a href={'#' + this.props.name}>{this.props.title}</a></h2>;
-  }
-});
-
-module.exports = SectionHeader;
